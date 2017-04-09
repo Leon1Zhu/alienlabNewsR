cf4385176076409c3dc1b02999c5f91d4da8228d
zhuliang <937154020@qq.com>
2017-04-09 12:35:32 +0800

    多页面更新

--- src/components/mainPage/mainPageThrContent/mainPageThrContent.vue
+++ src/components/mainPage/mainPageThrContent/mainPageThrContent.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="main-thr-content">
+    <img class="background-img" src="../../../assets/thrBackground.jpg">
        <div class="thr-title">
            <span class="straight thr-left-straight">━━━━</span>
            <span class="my-collect font-size-maintitle">我的收藏</span>
